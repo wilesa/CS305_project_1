ed9439501363e21f06e57f6d9d935d12ae72b5ba
wilesa <austinwiles95@gmail.com>
2017-02-16 23:11:09 -0500

    Server wouldnt compile, i left it commented below

--- HTTP.java
+++ HTTP.java
@@ -18,20 +18,35 @@ public class HTTP {
         this.start_line = "GET /" + url + " HTTP/" + this.version;
     }
 
-    public void ok() {
+    public void set_ok() {
         this.start_line = "HTTP/" + this.version + " 200 OK";
     }
 
-    public void not_modified() {
+    public void set_not_modified() {
         this.start_line = "HTTP/" + this.version + " 304 Not Modified";
     }
 
-    public void not_found() {
+    public void set_not_found() {
         this.start_line = "HTTP/" + this.version + " 404 Not Found";
     }
 
-    public void if_modified(String date) {
+    public void set_if_modified(String date) {
         this.headers.add("If-Modified-Since: " + date);
+    }
+
+    public void set_content(String content){
+        this.content = content;
+    }
+
+    public String get_status(){return this.start_line;}
+    public ArrayList<String> get_headers(){return this.headers;}
+    public String get_content(){return this.content;}
+
+    public String toString(){
+        if(this.start_line == null) return null;
+        String http = this.start_line + "\n";
+        for(String s : this.headers) http = http + s + "\n";
+        if(this.content != null) http = http + "\n" + this.content;
     }
 
 }
