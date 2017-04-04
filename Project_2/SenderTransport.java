f103f3464f683ad31b9eb81f645bf0f72733aa1b
John Plumb <plumbj@aec515ld-03.cs.lafayette.edu>
2017-04-03 20:36:59 -0400

    adding project 2

---
+++ Project_2/SenderTransport.java
@@ -0,0 +1,53 @@
+
+import java.util.ArrayList;
+/**
+ * A class which represents the receiver transport layer
+ */
+public class SenderTransport
+{
+    private NetworkLayer nl;
+    private Timeline tl;
+    private int n;
+    private boolean usingTCP;
+
+    public SenderTransport(NetworkLayer nl){
+        this.nl=nl;
+        initialize();
+
+    }
+
+    public void initialize()
+    {
+    }
+
+    public void sendMessage(Message msg)
+    {
+    }
+
+    public void receiveMessage(Packet pkt)
+    {
+    }
+
+    public void timerExpired()
+    { 
+    }
+
+    public void setTimeLine(Timeline tl)
+    {
+        this.tl=tl;
+    }
+
+    public void setWindowSize(int n)
+    {
+        this.n=n;
+    }
+
+    public void setProtocol(int n)
+    {
+        if(n>0)
+            usingTCP=true;
+        else
+            usingTCP=false;
+    }
+
+}
