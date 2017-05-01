96ef97bca39843c1a5c8cabce914cc7427d7bd7a
wilesa <austinwiles95@gmail.com>
2017-05-01 13:14:29 -0400

    making project 3

---
+++ project3/src/Controller.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Austin on 5/1/2017.
+ */
+public class Controller {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ project3/src/RIP.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Austin on 5/1/2017.
+ */
+public class RIP {
+}
